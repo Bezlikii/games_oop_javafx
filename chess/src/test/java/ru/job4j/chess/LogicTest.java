dadc3af5947ef39c4be3d897896dc31236bb5361
Mkrukov <mcshake1000@gmail.com>
2024-06-13 11:43:44 +0300

    3. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogicTest {
+
+    @Test
+    public void whenMoveThenFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    void whenMoveThenOccupiedCellException() throws OccupiedCellException {
+        Logic logic = new Logic();
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Figure bishopBlack2 = new BishopBlack(Cell.G5);
+        logic.add(bishopBlack);
+        logic.add(bishopBlack2);
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> logic.move(Cell.C1, Cell.G5));
+        assertThat(exception.getMessage()).isEqualTo("This diagonal is busy.");
+    }
+}
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.chess.firuges.black;
+
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class BishopBlackTest {
+
+    @Test
+    void checkPosition() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell result = bishopBlack.position();
+        assertThat(result).isEqualTo(Cell.C1);
+    }
+
+    @Test
+    void checkCopy() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Figure copiedBishop = bishopBlack.copy(Cell.G5);
+        Cell result = copiedBishop.position();
+        assertThat(result).isEqualTo(Cell.G5);
+        assertThat(bishopBlack.position()).isEqualTo(Cell.C1);
+    }
+
+    @Test
+    void checkWay() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] result = bishopBlack.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void checkDiagonalIfBusy() {
+        Figure bishopBlack = new BishopBlack(Cell.C1);
+        Cell result = Cell.F6;
+        assertThrows(ImpossibleMoveException.class, () -> {
+            bishopBlack.way(result);
+        });
+    }
+}
